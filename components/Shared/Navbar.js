885aff1944bef43106ceb3c9861ea8305b833b84
RAKIBUL <rakibulmd933@gmail.com>
2023-03-27 23:21:16 +0600

    [ADD] navbar completed

--- components/Shared/Navbar.js
+++ components/Shared/Navbar.js
@@ -28,6 +28,17 @@ function Navbar() {
 				</div>
 			</div>
 			<div className="navbar-end">
+				<Link className=" mx-5" href='/dashboard' >Dashboard</Link>
+				<label tabIndex={0} className="ml-1 btn btn-ghost btn-circle">
+					<div className="indicator">
+						<label tabIndex={0} className="btn btn-ghost btn-circle">
+							<div className="indicator">
+								<svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
+								<span className="badge badge-sm indicator-item">9</span>
+							</div>
+						</label>
+					</div>
+				</label>
 				<Link className=" mx-5" href='/login' >Login <ArrowForwardOutlinedIcon /> </Link>
 			</div>
 
