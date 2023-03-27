5ddf0bb958c11c191e945d2d7f888ac8b6ca77c8
RAKIBUL <rakibulmd933@gmail.com>
2023-03-28 01:15:20 +0600

    [ADD] slider completed

--- components/Banner.js
+++ components/Banner.js
@@ -10,10 +10,6 @@ function Banner() {
 		<div className="carousel w-full">
 			<div id="slide1" className="carousel-item relative w-full">
 				<Image src={image1} className="w-full" />
-				{/* <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-					<a href="#slide4" className="btn btn-circle">❮</a>
-					<a href="#slide2" className="btn btn-circle">❯</a>
-				</div> */}
 				<div className="absolute w-fit left-0 right-0 mx-auto flex gap-8 bottom-10">
 					<a href="#slide4" className="w-4 h-12 p-6 border-2 border-gray-700 flex items-center justify-center hover:bg-gray-700 hover:text-white active:bg-gray-900 duration-300">
 						<ArrowBackOutlinedIcon />
@@ -22,6 +18,13 @@ function Banner() {
 					<a href="#slide2" className="w-4 h-12 p-6 border-2 border-gray-700 flex items-center justify-center hover:bg-gray-700 hover:text-white active:bg-gray-900 duration-300">
 						<ArrowForwardOutlinedIcon />
 					</a>
+				</div>
+				<div className=" absolute left-10 bottom-48 p-6">
+					<h2 className=" font-normal text-3xl">LIFESTYLE COLLECTION</h2>
+					<h1 className=' font-extrabold text-6xl'>WOMAN</h1>
+					<h2 className=' font-semibold text-2xl'>SALE UP TO <span className=' text-red-500'>35% OFF</span></h2>
+					<h4 className=' font-light text-lg'>Get Free Shipping on orders over $99.00</h4>
+					<button className=" px-10 py-3 bg-black text-white mt-8">Shop Now</button>
 				</div>
 			</div>
 			<div id="slide2" className="carousel-item relative w-full">
@@ -35,6 +38,13 @@ function Banner() {
 						<ArrowForwardOutlinedIcon />
 					</a>
 				</div>
+				<div className=" absolute left-10 bottom-48 p-6">
+					<h2 className=" font-normal text-3xl">LIFESTYLE COLLECTION</h2>
+					<h1 className=' font-extrabold text-6xl'>MEN</h1>
+					<h2 className=' font-semibold text-2xl'>SALE UP TO <span className=' text-red-500'>35% OFF</span></h2>
+					<h4 className=' font-light text-lg'>Get Free Shipping on orders over $99.00</h4>
+					<button className=" px-10 py-3 bg-black text-white mt-8">Shop Now</button>
+				</div>
 			</div>
 			<div id="slide3" className="carousel-item relative w-full">
 				<Image src={image1} className="w-full" />
@@ -47,6 +57,13 @@ function Banner() {
 						<ArrowForwardOutlinedIcon />
 					</a>
 				</div>
+				<div className=" absolute left-10 bottom-48 p-6">
+					<h2 className=" font-normal text-3xl">LIFESTYLE COLLECTION</h2>
+					<h1 className=' font-extrabold text-6xl'>WOMAN</h1>
+					<h2 className=' font-semibold text-2xl'>SALE UP TO <span className=' text-red-500'>35% OFF</span></h2>
+					<h4 className=' font-light text-lg'>Get Free Shipping on orders over $99.00</h4>
+					<button className=" px-10 py-3 bg-black text-white mt-8">Shop Now</button>
+				</div>
 			</div>
 			<div id="slide4" className="carousel-item relative w-full">
 				<Image src={image} className="w-full" />
@@ -58,6 +75,13 @@ function Banner() {
 					<a href="#slide3" className="w-4 h-12 p-6 border-2 border-gray-700 flex items-center justify-center hover:bg-gray-700 hover:text-white active:bg-gray-900 duration-300">
 						<ArrowForwardOutlinedIcon />
 					</a>
+				</div>
+				<div className=" absolute left-10 bottom-48 p-6">
+					<h2 className=" font-normal text-3xl">LIFESTYLE COLLECTION</h2>
+					<h1 className=' font-extrabold text-6xl'>MEN</h1>
+					<h2 className=' font-semibold text-2xl'>SALE UP TO <span className=' text-red-500'>35% OFF</span></h2>
+					<h4 className=' font-light text-lg'>Get Free Shipping on orders over $99.00</h4>
+					<button className=" px-10 py-3 bg-black text-white mt-8">Shop Now</button>
 				</div>
 			</div>
 		</div>
