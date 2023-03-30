d2ed5848ea7a5a82b9037437a40f5f2177ac7c16
RAKIBUL <rakibulmd933@gmail.com>
2023-03-30 16:49:53 +0600

    [ADD] search bar

--- app/search/page.js
+++ app/search/page.js
@@ -1,8 +1,27 @@
-import React from 'react'
+"use client";
+import React from 'react';
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 
 function Search() {
 	return (
-		<div>Search Page</div>
+		<div className='h-screen p-4 bg-slate-200'>
+			<div className="p-4 m-4 rounded-md bg-slate-100">
+				<div className="flex items-center justify-center ">
+					<div className="flex items-center justify-center ">
+						<input type="search" placeholder="Searching for ..." className="w-full px-5 py-2 pr-10 mr-3 text-sm bg-white border rounded-full focus:outline-none" />
+						<div className="absolute top-0 left-0 flex justify-center pl-1 mt-3 mr-4 item-center">
+							<SearchOutlinedIcon fontSize="small" />
+						</div>
+						<button className='px-5 py-2 bg-teal-500 rounded'>Search</button>
+					</div>
+				</div>
+			</div>
+			<div className="p-6 m-4 bg-white rounded-md">
+				<div className="">
+					<h2> World</h2>
+				</div>
+			</div>
+		</div>
 	)
 }
 
--- components/Shared/Navbar.js
+++ components/Shared/Navbar.js
@@ -1,7 +1,7 @@
 "use client"
 import Link from "next/link";
 import ArrowForwardOutlinedIcon from '@mui/icons-material/ArrowForwardOutlined';
-import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+
 
 
 function Navbar() {
@@ -10,25 +10,19 @@ function Navbar() {
 			<div className="navbar-start">
 				<div className="dropdown">
 					<label tabIndex={0} className="btn btn-ghost lg:hidden">
-						<svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+						<svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
 					</label>
-					<ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+					<ul tabIndex={0} className="p-2 mt-3 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52">
 						<li><Link href='/products'>Products</Link></li>
 						<li><Link href='/about'>About</Link></li>
 						<li><Link href='/contact'>Contacts</Link></li>
 						<li><Link href='/dashboard'>Dashboard</Link></li>
 					</ul>
 				</div>
-				<a className="btn btn-ghost normal-case text-xl">Walker Shop</a>
-			</div>
-			<div className="navbar-center lg:w-2/5 lg:flex relative">
-				<input type="search" placeholder="Searching for ..." className="bg-white w-96 h-10 px-5 pr-10 rounded-full text-sm focus:outline-none" />
-				<div className="pl-1 absolute left-0 top-0 mt-3 mr-4 flex justify-center item-center">
-					<SearchOutlinedIcon fontSize="small" />
-				</div>
+				<a className="text-xl normal-case btn btn-ghost">Walker Shop</a>
 			</div>
 			<div className="navbar-end">
-				<Link className=" mx-5" href='/dashboard' >Dashboard</Link>
+				<Link className="mx-5 " href='/dashboard' >Dashboard</Link>
 				<label tabIndex={0} className="ml-1 btn btn-ghost btn-circle">
 					<div className="indicator">
 						<label tabIndex={0} className="btn btn-ghost btn-circle">
@@ -39,7 +33,7 @@ function Navbar() {
 						</label>
 					</div>
 				</label>
-				<Link className=" mx-5" href='/login' >Login <ArrowForwardOutlinedIcon /> </Link>
+				<Link className="mx-5 " href='/login' >Login <ArrowForwardOutlinedIcon /> </Link>
 			</div>
 
 		</div>
