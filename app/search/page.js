aa5ea66641f78c94a225cc1ba4570b08afcd7541
RAKIBUL <rakibulmd933@gmail.com>
2023-03-30 17:45:49 +0600

    [ADD] search

--- app/search/page.js
+++ app/search/page.js
@@ -5,11 +5,21 @@ import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 function Search() {
 	return (
 		<div className='h-screen p-4 bg-slate-200'>
-			<div className="p-4 m-4 rounded-md bg-slate-100">
+			<div className="p-4 m-4 rounded-md">
 				<div className="flex items-center justify-center ">
-					<div className="flex items-center justify-center w-1/2 ">
-						<input type="search" placeholder="Searching for ..." className="w-full px-5 py-2 pr-10 mr-3 text-sm bg-white border rounded-full focus:outline-none" />
-						<div className="absolute top-0 left-0 flex justify-center pl-1 mt-3 mr-4 item-center">
+					<div className="absolute flex items-center justify-center w-1/2 ">
+						<div className="flex flex-row w-full gap-0 mr-3 text-sm bg-white border rounded-full ">
+							<input type="search" placeholder="Searching for ..." className="w-full py-2 pl-5 pr-2 rounded-md focus:outline-none" />
+							<select className='rounded cursor-pointer'>
+								<option value="name">Electronic</option>
+								<option value="name">Man</option>
+								<option value="name">Woman</option>
+								<option value="name">Child</option>
+								<option value="name">Food</option>
+								<option value="name">Others</option>
+							</select>
+						</div>
+						<div className="absolute left-0 flex items-center justify-center ml-1 top-3">
 							<SearchOutlinedIcon fontSize="small" />
 						</div>
 						<button className='px-5 py-2 bg-teal-500 rounded'>Search</button>
