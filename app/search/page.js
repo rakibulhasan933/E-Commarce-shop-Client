45ea5d86c714b2f13d3d8d0760c1f43a5911cf10
RAKIBUL <rakibulmd933@gmail.com>
2023-03-30 17:58:08 +0600

    [ADD] search bar responsive

--- app/search/page.js
+++ app/search/page.js
@@ -7,10 +7,11 @@ function Search() {
 		<div className='h-screen p-4 bg-slate-200'>
 			<div className="p-4 m-4 rounded-md">
 				<div className="flex items-center justify-center ">
-					<div className="absolute flex items-center justify-center w-1/2 ">
+					<div className="absolute flex md:w-1/2 md:items-center md:justify-center ">
 						<div className="flex flex-row w-full gap-0 mr-3 text-sm bg-white border rounded-full ">
-							<input type="search" placeholder="Searching for ..." className="w-full py-2 pl-5 pr-2 rounded-md focus:outline-none" />
+							<input type="search" placeholder="Searching for ..." className="w-full py-2 pl-5 pr-2 rounded-md focus:outline-none " />
 							<select className='rounded cursor-pointer'>
+								<option value="name">All Category</option>
 								<option value="name">Electronic</option>
 								<option value="name">Man</option>
 								<option value="name">Woman</option>
