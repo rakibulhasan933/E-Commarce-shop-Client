b3cb0d8f489c6789fafe4cc1a3b7f33dc1727323
RAKIBUL <rakibulmd933@gmail.com>
2023-03-30 16:52:21 +0600

    [ADD] search bar

--- app/search/page.js
+++ app/search/page.js
@@ -7,7 +7,7 @@ function Search() {
 		<div className='h-screen p-4 bg-slate-200'>
 			<div className="p-4 m-4 rounded-md bg-slate-100">
 				<div className="flex items-center justify-center ">
-					<div className="flex items-center justify-center ">
+					<div className="flex items-center justify-center w-1/2 ">
 						<input type="search" placeholder="Searching for ..." className="w-full px-5 py-2 pr-10 mr-3 text-sm bg-white border rounded-full focus:outline-none" />
 						<div className="absolute top-0 left-0 flex justify-center pl-1 mt-3 mr-4 item-center">
 							<SearchOutlinedIcon fontSize="small" />
